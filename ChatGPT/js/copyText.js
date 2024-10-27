91dafcf463fa3ed8c4ea783f031c70c71ecd0309
Meraz Alvee <merazalveego@gmail.com>
2024-10-28 01:08:17 +0600

    Update copyText.js

--- ChatGPT/js/copyText.js
+++ ChatGPT/js/copyText.js
@@ -5,6 +5,10 @@
 *     Version: 1.3
 *     License: MIT License (https://opensource.org/licenses/MIT)
 *     GitHub Repository: https://github.com/merazalveego/ChatGPT/js/copyText.js
+*
+*     Use Copy Text ---
+*     <p id="textToCopy">This is the text to copy.</p>
+*     <button onclick="copyText('textToCopy')">Copy Text</button>
 */
 
 function copyText(elementId) {var text = document.getElementById(elementId).innerText;navigator.clipboard.writeText(text).then(function () {alert("Text copied to clipboard!");}).catch(function (error) {alert("Failed to copy text: " + error);});}
